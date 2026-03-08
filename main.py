328131e97146ab5cacb630c18781ccfe1f715fb3
BogdanSpasov <bogspasov@abv.bg>
2026-03-08 22:51:39 +0200

    Revert "Movie Library Management API"
    This reverts commit 32a6191518d68180a9a9ea1124373c7cf8e40af4.

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routers.users import users_router
-from routers.movies import movies_router
-
-
-app = FastAPI()
-app.include_router(movies_router)
-app.include_router(users_router)
